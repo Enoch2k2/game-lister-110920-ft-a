0612f97c5a672476ecae77187f9abed29df02d9e
Enoch2k2 <enoch2k2@hotmail.com>
2020-12-22 11:07:23 -0600

    added game controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,3 +1,26 @@
 class GamesController < ApplicationController
+  get '/games' do
+    @games = Game.all
+    erb :'games/index'
+  end
+
   
+  get '/games/new' do
+    erb :'games/new'
+  end
+  
+  get '/games/:id' do
+    @game = Game.find_by_id(params[:id])
+    erb :'games/show'
+  end
+
+  post '/games' do
+    game = Game.new(params[:game])
+
+    if game.save
+      redirect '/games'
+    else
+      redirect '/games/new'
+    end
+  end
 end
