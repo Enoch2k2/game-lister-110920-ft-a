8f3ed191094d1cb38bfa7fd9b340c62c9385b4ca
Enoch2k2 <enoch2k2@hotmail.com>
2020-12-24 11:07:20 -0600

    added users and game_id to games

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20201224162627) do
+ActiveRecord::Schema.define(version: 20201224162738) do
 
   create_table "games", force: :cascade do |t|
     t.string   "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20201224162627) do
     t.string   "genre"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
